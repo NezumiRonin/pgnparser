ff31c8537eed5808906b666f88a5cf5d88b889b3
Alex Gómez <nezumironin@gmail.com>
2020-11-29 19:31:45 -0600

    Optimizaciones finales

--- README.md
+++ README.md
@@ -6,31 +6,52 @@ A simple PGN Parser with tools. **MADE WITH PERL!**
 It contains a set of tools to process the games in a PGN file.
 
 ## Install
-Just copy the perl script to your Linux computer.
-If you use somehow still use Windows, you can get [Linux Mint](https://linuxmint.com/).
-If you insist in using Windows, you can install [Strawberry Perl](http://strawberryperl.com/).
+* Just copy the perl script to your Linux computer.
+* If you use somehow still use Windows, you can get [Linux Mint](https://linuxmint.com/).
+* If you insist in using Windows, you can install [Strawberry Perl](http://strawberryperl.com/).
 	
 
-## Usaga
+## Usage
 ```shell
 ./pgnparser.pl COMMAND file [options]
 ```
 
 ### Commands
-search: Searchs a pattern
+#### search
+Searchs a pattern in the game position.
 
-
-j: it's a special "detail" character and stands for Jump, to ignore other pieces in the move path.
+* **w**	Evaluate only *w*hite or *b*lack active move, must be first.
+* **Qd1** Queen in 'd1'
+* **Qma4** Queen can *m*ove/capture to 'a4', valid move with free path.
+* **Qxd8** Queen can *x*capture to 'd8', destination square must be with enemy piece.
+* **Qjh5** Queen can *j*ump to 'h5', ignore other pieces in path.
+* **.a8**  Any piece on 'a8' (To be done)
+* **!h8**  No piece on 'h8' (To be done)
 Think "short-circuit" evaluation, faster and/or lesser common positions go first for speed.
-Example for 'Greek gift sacrifice':
+
+Example for 'King Usurper', white king on black throne:
 ```shell
-./pgnparser.pl search file.pgn 'w Bxh7 Ng5 Qjh5'
+./pgnparser.pl search file.pgn 'w Ke8'
 ```
 
-tofen: Converts games to FEN.
-Example:
+Example for 'Greek gift sacrifice', white turn:
 ```shell
-./pgnparser.pl search file.pgn
+./pgnparser.pl search file.pgn 'w kg8 rf8 Bxh7 Nmg5 Qjh5'
 ```
+
+
+## TO DO
+* Convert game to FEN.
+* Convert game to images.
+* Reformat file nice.
+* Code optimization.
+* Nice option parser.
+* Make GIF of the game.
+
+
+## NOTES
+* Made in week coding sprint, around 40 hours (because pandemic).
+* Nov 28, 2020: ./pgnparser.pl justplay smyslov_2627.pgn -> 2627 games processed in 75s (35.0 gps)
+* Nov 29, 2020: ./pgnparser.pl justplay smyslov_2627.pgn -> 2627 games processed in 68s (38.6 gps)
 
 
--- pgnparser.pl
+++ pgnparser.pl
@@ -1,5 +1,4 @@
 #!/usr/bin/perl
-
 # PGN Parser v0.1
 #===================
 # November 25, 2020
@@ -8,12 +7,7 @@
 
 # CONFIGURATION SECTION
 #=======================
-
-
-# TO DO:
-#=======
-# Check for valid castling, passant and on check
-
+my $Pedantic= 1;
 
 
 
@@ -22,132 +16,169 @@
 ########
 use strict;
 my $Lapse= time;
-my %Board;
 my $GameNum;
-my @GameLin;
+my $GameLin;
+my %Search;
+my @Pattern;
+
+my $FH;
+my %GAME;
+my %BOARD;
 my %Allow= AllowInit();
 
 
+#REM: PROCESS...
 my $Proc= $ARGV[0];
 my $File= $ARGV[1];
-
-
-open(my $FH, $File) or die "ERR: $!($File)\n";
+if ($Proc eq "justplay") { &JustPlay(); }
 if ($Proc eq "search" && $ARGV[2]) { &Search($ARGV[2]); }
-if ($Proc eq "tofen") { &ToFen(); }
-close($FH);
 
 
+#REM: Finally...
 $Lapse= time-$Lapse;
-my $Persec= $GameNum==0 ? 0 : sprintf("%.1f", $GameNum/$Lapse);
-
-print "$GameNum games processed in $Lapse\s ($Persec gps)";
+my $Persec= $GameNum>0 ? sprintf("%.1f", $Lapse>0 ? $GameNum/$Lapse : 0) : 0;
+print "$GameNum games processed in $Lapse\s ($Persec gps)\n";
 exit;
 
 
+############
+# COMMANDS #
+############
 
-#==> THE INMORTAL GAME: Anderssen - Kieseritzky (London 1851):
-my $InmortalPNG= qq|
-	1 e4 e5 2 f4 exf4 3 Bc4 Qh4+ 4 Kf1 b5 5 Bxb5 Nf6 6 Nf3 Qh6 7 d3 Nh5 8 Nh4 Qg5
-	9 Nf5 c6 10 Rg1 cxb5 11 g4 Nf6 12 h4 Qg6 13 h5 Qg5 14 Qf3 Ng8 15 Bxf4 Qf6
-	16 Nc3 Bc5 17 Nd5 Qxb2 18 Bd6 Qxa1+ 19 Ke2 Bxg1 20 e5 Na6 21 Nxg7+ Kd8
-	22 Qf6+ Nxf6 23 Be7 Checkmate.
-	|;
-
-
-#==> TUROCHAMP: Turing's paper machine – Alick Glennie, Manchester 1952:
-my $TuroChamp= qq|
-	1.e4 e5 2.Nc3 Nf6 3.d4 Bb4 4.Nf3 d6 5.Bd2 Nc6 6.d5 Nd4 7.h4 Bg4 8.a4 Nxf3+
-	9.gxf3 Bh5 10.Bb5+ c6 11.dxc6 0-0 12.cxb7 Rb8 13.Ba6 Qa5 14.Qe2 Nd7
-	15.Rg1 Nc5 16.Rg5 Bg6 17.Bb5 Nxb7 18.0-0-0 Nc5 19.Bc6 Rfc8 20.Bd5 Bxc3
-	21.Bxc3 Qxa4 22.Kd2 Ne6 23.Rg4 Nd4 24.Qd3 Nb5 25.Bb3 Qa6 26.Bc4 Bh5
-	27.Rg3 Qa4 28.Bxb5 Qxb5 29.Qxd6 Rd8 0-1.
-	|;
-
-#Play($Png);
-
-
-###########
-# PROCESS #
-###########
-
-sub Search {
-	my ($Strpos)= @_;
+sub JustPlay {
 	while(1) {
-		my %Game= &NextGame();
-		if ($Game{MOVES} eq "") { last; }
-		$GameNum++;
-		#print "==> GAME #$GameNum\n";
-		&Play($Game{MOVES});
-		#print "\n\n\n";
-		}
-	}
-
-
-sub PrintGame {
-	my (%Game)= @_;
-	print "Event: $Game{Event}\n";
-	print "Site: $Game{Site}\n";
-	print "Date: $Game{Date}\n";
-	print "Round: $Game{Round}\n";
-	print "White: $Game{White}\n";
-	print "Black: $Game{Black}\n";
-	print "Result: $Game{Result}\n";
-
-	my $Count;
-	my @Moves= &ParseMoves($Game{MOVES});
-	foreach my $Mov (@Moves) {
-		my ($W, $B)= split(" ", $Mov);
-		$Count++;
-		print "$Count\t$W\t$B\n";
+		&NextGame();
+		if ($GAME{MOVES} eq "") { last; }
+		&Play();
 		}
 	return;
 	}
 
 
+sub Search {
+	my ($Str)= @_;
+	@Pattern= split(" ", $Str);
+	while(1) {
+		$Search{MoveCount}=0;
+		&NextGame();
+		if ($GAME{MOVES} eq "") { last; }
+		&Play($GAME{MOVES});
+		}
+	}
+
+
+########
+# FILE #
+########
+
 sub NextGame {
-	my %Tags;
+	%GAME= ();
+	my $Start=1;
+
+	if ($GameLin==0) { open($FH, $File) or die "ERR: $!($File)\n"; }
 	while(my $Line=<$FH>) {
+		$GameLin++;
 		$Line=~ s/\r//g;
 		$Line=~ s/\n//g;
 		$Line=~ s/\t/ /g;
 		$Line=~ s/\s+/ /g;
 		$Line=~ s/^\s+//;
 		$Line=~ s/\s+$//;
-
-		if (length($Line)<3) {
-			if ($Tags{MOVES}) { last; }
-			next;
-			}
+		$Line=~ s/^;.*$//;
 
 		if ($Line=~ /^\[(.*)\]$/) {
+			if ($Start==1) {
+				$GAME{GameNum}= ++$GameNum;
+				$GAME{GameLin}= $GameLin;
+				$Start=0;
+				}
 			my ($Key, $Value)= split(/ \"/, $1);
 			chop $Value;
-			$Tags{$Key}= $Value;
+			$GAME{$Key}= $Value;
 			next;
 			}
-		
-		$Tags{MOVES}.= $Line." ";
+
+		if (length($Line)<3) {
+			if ($GAME{MOVES}) { last; }
+			next;
+			}
+		$GAME{PRIME}.= $Line." ";
+		&ParsePrime();
 		}
-	return %Tags;
+	if (eof($FH)) { close($FH); }
+	return;
+	}
+
+
+sub ParsePrime {
+	my $Pgn= $GAME{PRIME};
+	
+	#REM: Remove balanced pesky comentaries...
+	#$Pgn=~ s/\{.*\}/ /g;
+	my @Balance;
+	my $Pos1;
+	my $Pos2;
+	for (my $J=0; $J<length($Pgn); $J++) {
+		my $Char= substr($Pgn, $J, 1);
+		if ($Char=~ /[\{\(]/) {
+			if (scalar(@Balance)==0) { $Pos1= $J; }
+			push @Balance, $Char;
+			}
+		if ($Char=~ /[\}\)]/) {
+			if (scalar(@Balance)==1) {
+				$Pos2= $J+1;
+				substr($Pgn, $Pos1, $Pos2-$Pos1) = " " x ($Pos2-$Pos1);
+				}
+			pop @Balance
+			}
+		}
+
+	$Pgn=~ s/\$\d+/ /g; #I dont't know what $n means...	
+	$Pgn=~ s/\d+\.\.\./ /g; #Remove elipsis...
+	$Pgn=~ s/\./ /g;
+	$Pgn=~ s/\s+/ /g;
+	$GAME{MOVES}= $Pgn;
+
+	my @Moves;
+	my @Parts= split(" ", $Pgn);
+	for (my $J; $J<@Parts; $J+=3) {
+		my $Wht= $Parts[$J+1];
+		my $Blk= $Parts[$J+2];
+		
+		if ($Wht=~ /^[0-9]/) {$Wht=""; }
+		if ($Blk=~ /^[0-9]/) {$Blk=""; }
+		if ($Wht eq "" && $Blk eq "") { last; }
+		push @Moves, "$Wht $Blk";
+		}
+	$GAME{ARRAY}= \@Moves;
+	return;
 	}
 
 
 ############
 # GAMEPLAY #
 ############
+sub BoardInit {
+	%BOARD= ();
+	$BOARD{FLAG}{active}="w";
+	$BOARD{FLAG}{castling}="KQkq";
+	$BOARD{FLAG}{passant}="-";
+	$BOARD{FLAG}{halfmove}= 0;
+	$BOARD{FLAG}{fullmove}= 1;
+	%BOARD= (%BOARD, a8=>"r", b8=>"n", c8=>"b", d8=>"q", e8=>"k", f8=>"b", g8=>"n", h8=>"r");
+	%BOARD= (%BOARD, a7=>"p", b7=>"p", c7=>"p", d7=>"p", e7=>"p", f7=>"p", g7=>"p", h7=>"p");
+	%BOARD= (%BOARD, a2=>"P", b2=>"P", c2=>"P", d2=>"P", e2=>"P", f2=>"P", g2=>"P", h2=>"P");
+	%BOARD= (%BOARD, a1=>"R", b1=>"N", c1=>"B", d1=>"Q", e1=>"K", f1=>"B", g1=>"N", h1=>"R");
+	return;
+	}
+
+
 sub AllowInit {
 	my %Allow;
-	# Direction and Distance
-	# 0, 45, 90, 180, etc...
 
-	#REM: Pawn special moves: 1 square, 2 square, right capture, left capture.
+	#REM: Pawn special moves: 1 square, 2 squares, right capture, left capture.
 	$Allow{P}= ['0,+1', '0,+2', '+1,+1', '-1,+1'];
 	$Allow{p}= ['0,-1', '0,-2', '-1,-1', '+1,-1'];
-
-	#REM: Pawn extra "Allow" added later if possible...
-	#$Allow{P}= ['0,+1'];
-	#$Allow{p}= ['0,-1'];
 
 	$Allow{N}= ['+1,+2', '+2,+1', '+2,-1', '+1,-2', '-1,-2', '-2,-1', '-2,+1', '-1,+2'];
 
@@ -168,81 +199,17 @@ sub AllowInit {
 	}
 
 
-sub BoardInit {
-	%Board= ();
-	$Board{FLAG}{active}="w";
-	$Board{FLAG}{castling}="KQkq";
-	$Board{FLAG}{passant}="-";
-	$Board{FLAG}{halfmove}="0";
-	$Board{FLAG}{fullmove}="";
-	%Board= (%Board, a8=>"r", b8=>"n", c8=>"b", d8=>"q", e8=>"k", f8=>"b", g8=>"n", h8=>"r");
-	%Board= (%Board, a7=>"p", b7=>"p", c7=>"p", d7=>"p", e7=>"p", f7=>"p", g7=>"p", h7=>"p");
-	%Board= (%Board, a2=>"P", b2=>"P", c2=>"P", d2=>"P", e2=>"P", f2=>"P", g2=>"P", h2=>"P");
-	%Board= (%Board, a1=>"R", b1=>"N", c1=>"B", d1=>"Q", e1=>"K", f1=>"B", g1=>"N", h1=>"R");
-	return;
-	}
-
-
-sub BoardShow {
-	binmode(STDOUT, ":utf8");
-	print " -----------------\n";
-	for (my $Row=8; $Row>0; $Row--) {
-		print "$Row|";
-		foreach my $Col(("a".."h")) {
-			my $Piece=$Board{$Col.$Row} ? $Board{$Col.$Row} : " ";
-			if ($^O=~ /linux/i) {
-				$Piece=~ tr/KQRBNP/\N{U+2654}\N{U+2655}\N{U+2656}\N{U+2657}\N{U+2658}\N{U+2659}/;
-				$Piece=~ tr/kqrbnp/\N{U+265A}\N{U+265B}\N{U+265C}\N{U+265D}\N{U+265E}\N{U+265F}/;
-				}
-			print "$Piece|";
-			}
-		print "\n"; #-----------------\n";
-		}
-	print " -----------------\n";
-	print "  a b c d e f g h\n";
-
-	}
-
-
-sub ParseMoves {
-	my ($Png)=@_;
-	$Png=~ s/\{.*\}//g; #Remove pesky comentaries...
-	$Png=~ s/\./ /g;
-
-	my @Moves;
-	my @Parts= split(" ", $Png);
-	for (my $J; $J<@Parts; $J+=3) {
-		my $Wht= $Parts[$J+1];
-		my $Blk= $Parts[$J+2];
-		
-		if ($Wht=~ /^[0-9]/) {$Wht=""; }
-		if ($Blk=~ /^[0-9]/) {$Blk=""; }
-		if ($Wht eq "" && $Blk eq "") { last; }
-		push @Moves, "$Wht $Blk";
-		}
-	return @Moves;
-	}
-
-
 sub Play {
-	my ($Moves)= @_;
-
-	my @Board= &BoardInit();
-	#BoardShow();
-	
-	my @Game= &ParseMoves($Moves);
-
+	my @Board= &BoardInit();	
 	my $Count=0;
-	for my $Mov (@Game) {
+	for my $Mov (@{$GAME{ARRAY}}) {
 		$Count++;
-		my ($W, $B)= split(" ", $Mov);
-		#print "$Count\.\t$W\t$B\n";
-		&Move($W);
-		#&BoardShow();
-		if ($B eq "") { last; }
-		&Move($B);
-		#&BoardShow();
+		my ($Wht, $Blk)= split(" ", $Mov);
+		&Move($Wht);
+		if ($Blk eq "") { last; }
+		&Move($Blk);
 		}
+	return;
 	}
 
 
@@ -251,6 +218,8 @@ sub Move {
 	my $Move= $OriMove;
 
 	#==> Special Sigils...
+	$Move=~ s/\!//g;
+	$Move=~ s/\?//g;
 	my $Captu= 0;
 	my $Check= 0;
 	my $Cmate= 0;
@@ -259,17 +228,18 @@ sub Move {
 	if ($Move=~ /\+$/) { $Check++; $Move=~ s/\+$//; }
 	if ($Move=~ /\#$/) { $Cmate++; $Move=~ s/\#$//; }
 	if ($Move=~ /=([QRBN])/) { $Promo= $1; $Move=~ s/=.//; }
+	
 
 	#==> CASTLING:
 	if ($Move eq "O-O") {
-		if ($Board{FLAG}{active} eq "w") {
+		if ($BOARD{FLAG}{active} eq "w") {
 			BoardMove("e1", "g1"); return;
 			} else {
 			BoardMove("e8", "g8"); return;
 			}
 		}
 	if ($Move eq "O-O-O") {
-		if ($Board{FLAG}{active} eq "w") {
+		if ($BOARD{FLAG}{active} eq "w") {
 			BoardMove("e1", "c1");
 			return;
 			} else {
@@ -282,7 +252,7 @@ sub Move {
 	if ($Move=~ /^[a-h]/) { $Move= 'P'.$Move; }
 
 	#==> Lowercase black pieces...
-	if ($Board{FLAG}{active} eq "b") {
+	if ($BOARD{FLAG}{active} eq "b") {
 		my $Chng=lc(substr($Move, 0, 1));
 		$Move= $Chng.substr($Move, 1);
 		$Promo= lc($Promo);
@@ -303,18 +273,19 @@ sub Move {
 		}
 	if (scalar @Psb==1) {
 		BoardMove($Psb[0], $To);
-		if ($Promo) { $Board{$To}= $Promo; }
+		if ($Promo) { $BOARD{$To}= $Promo; }
 		return;
 		}
 
 	print "ERR: Couldn't make move ($OriMove)!\n";
-	print "DBG: Active=$Board{FLAG}{active} Piece=$Piece Deta=$Deta Captu=$Captu To=$To Promo=$Promo Psb=".scalar(@Psb)."\n";
+	print "DBG: Active=$BOARD{FLAG}{active} Piece=$Piece Deta=$Deta Captu=$Captu To=$To Promo=$Promo Psb=".scalar(@Psb)."\n";
+	&ShowData();
 	exit 666;
 	}
 
 
 sub Enpass {
-	my $Square= $Board{FLAG}{passant};
+	my $Square= $BOARD{FLAG}{passant};
 	if ($Square eq "-") { return ""; }
 	my $X= substr($Square, 0, 1);
 	if ($Square=~ /3$/) { return $X."4"; }
@@ -323,46 +294,13 @@ sub Enpass {
 	}
 
 
-sub BoardMove {
-	#REM: Makes move of pieces in the board and adjust flags.
-	my ($From, $To)= @_;
-
-	my $Piece= $Board{$From};
-	my $Steps= Steps($From, $To);
-	$Board{$To}= delete $Board{$From};
-	
-	#REM: Castling move rook.
-	if (uc($Piece) eq "K" && $Steps==2) {
-		if ($To eq "g1") { $Board{f1}= delete $Board{h1}; $Board{FLAG}{castling}=~ s/K//;}
-		if ($To eq "c1") { $Board{d1}= delete $Board{a1}; $Board{FLAG}{castling}=~ s/Q//;}
-		if ($To eq "g8") { $Board{f8}= delete $Board{h8}; $Board{FLAG}{castling}=~ s/k//;}
-		if ($To eq "c8") { $Board{d8}= delete $Board{a8}; $Board{FLAG}{castling}=~ s/q//;}
-		}
-
-	#REM: Passant move...
-	if (uc($Piece) eq "P" && $To eq $Board{FLAG}{passant}) {
-		delete $Board{&Enpass()};
-		}
-	$Board{FLAG}{passant}="-";
-	if (uc($Piece) eq "P" && $Steps==2) {
-		my $X= substr($From, 0, 1);
-		if ($From=~ /2$/) { $Board{FLAG}{passant}= $X."3"; }
-		if ($From=~ /7$/) { $Board{FLAG}{passant}= $X."6"; }
-		}
-
-	#REM: Toggle color turn...
-	$Board{FLAG}{active}= $Board{FLAG}{active} eq "w" ? "b" : "w";
-	return;
-	}
-
-
 sub WhereIs {
 	my ($Piece, $Deta)= @_;
 	my @Square;
-	foreach my $Pos (sort keys %Board) {
+	foreach my $Pos (keys %BOARD) {
 		if (length($Pos)!=2) { next; }
 		if ($Deta && $Pos!~ /$Deta/) { next; }
-		if ($Board{$Pos} eq $Piece) { push @Square, $Pos; }
+		if ($BOARD{$Pos} eq $Piece) { push @Square, $Pos; }
 		}
 	return @Square;
 	}
@@ -378,8 +316,18 @@ sub ToCoords {
 	if ($Fx<1 || $Fx>8) { return undef; }
 	if ($Fy<1 || $Fy>8) { return undef; }
 	$Fx=~ tr/12345678/abcdefgh/;
-	$Square= $Fx.$Fy;
-	return $Square;	
+	return $Fx.$Fy;	
+	}
+
+
+sub WhatColor {
+	my ($Piece)= @_;
+	#if ($Piece=~ /[KQRBNP]/) { return "w"; }
+	#if ($Piece=~ /[kqrbnp]/) { return "b"; }
+	if ($Piece eq "") {return ""; }
+	if ($Piece eq uc($Piece)) { return "w"; }
+	if ($Piece eq lc($Piece)) { return "b"; }
+	return "";
 	}
 
 
@@ -410,7 +358,7 @@ sub CheckPath {
 
 	#==> It's a Knight...
 	#Can use euclidian distance 1²+2²=5 always
-	if (uc($Board{$From}) eq "N" && $Steps==2) {
+	if (uc($BOARD{$From}) eq "N" && $Steps==2) {
 		return @Path;
 		}
 
@@ -423,24 +371,16 @@ sub CheckPath {
 		if ($Dy<0) { $Jy++;}
 		if ($Dy>0) { $Jy--;}
 		my $Pos= ToCoords($From, "$Jx,$Jy");
-		my $Pie= $Board{$Pos};
+		my $Pie= $BOARD{$Pos};
 		if ($Pie ne "") { push @Path, "$Pie$Pos"; }
 		}	
 	return @Path;
 	}
 
 
-sub WhatColor {
-	my ($Piece)= @_;
-	if ($Piece=~ /[KQRBNP]/) { return "w"; }
-	if ($Piece=~ /[kqrbnp]/) { return "b"; }
-	return "";
-	}
-
-
 sub Attacking {
 	my ($Square)= @_;
-	my $Piece= $Board{$Square};
+	my $Piece= $BOARD{$Square};
 	my @Enemies= qw(K Q R B N P);
 
 	my @AttFrom= ();
@@ -457,8 +397,8 @@ sub Attacking {
 
 
 sub CanMove {
-	my ($From, $To, $Nc)= @_;
-	my $Piece= $Board{$From};
+	my ($From, $To, $Nc, $Jmp)= @_;
+	my $Piece= $BOARD{$From};
 
 	#REM: Load legal moves for the piece.
 	my @Moves= $Piece eq "p" ? @{$Allow{'p'}} : @{$Allow{uc($Piece)}};
@@ -469,10 +409,10 @@ sub CanMove {
 		my $Step2= &ToCoords($From, $Moves[1]);
 		my $CaptR= &ToCoords($From, $Moves[2]);
 		my $CaptL= &ToCoords($From, $Moves[3]);
-		if ($Board{$Step1} ne "") { $Moves[0]=""; $Moves[1]=""; }
-		if ($Board{$Step2} ne "") { $Moves[1]=""; }
-		if ($Board{$CaptR} eq "" && $CaptR ne $Board{FLAG}{passant}) { $Moves[2]=""; }
-		if ($Board{$CaptL} eq "" && $CaptL ne $Board{FLAG}{passant}) { $Moves[3]=""; }
+		if ($BOARD{$Step1} ne "") { $Moves[0]=""; $Moves[1]=""; }
+		if ($BOARD{$Step2} ne "") { $Moves[1]=""; }
+		if ($BOARD{$CaptR} eq "" && $CaptR ne $BOARD{FLAG}{passant}) { $Moves[2]=""; }
+		if ($BOARD{$CaptL} eq "" && $CaptL ne $BOARD{FLAG}{passant}) { $Moves[3]=""; }
 		}
 
 	#REM: Try all posible moves.
@@ -480,21 +420,21 @@ sub CanMove {
 		if( $Try eq "") { next; }
 		my $Tto= &ToCoords($From, $Try);
 		if ($Tto eq "") { next; } #Out of board.
-		if (&WhatColor($Piece) eq &WhatColor($Board{$Tto})) { next; } #Square ocuppied by partner.
+		if (&WhatColor($Piece) eq &WhatColor($BOARD{$Tto})) { next; } #Square ocuppied by partner.
 		#DBG: print "Posible: $Piece $From($Try) -> $Tto\n" if $Nc==0;
-		if ($Tto eq $To && scalar(CheckPath($From, $To))==0) {
+		if ($Tto eq $To && ($Jmp==1 || scalar(CheckPath($From, $To))==0)) {
 			my $Check= 0;
 			if ($Nc==0) {
 				my $King= "K";
 				if (&WhatColor($Piece) eq "b") { $King= "k";}
-				my %Save= %Board;
-				$Board{$To}= delete $Board{$From};
-				if (uc($Piece) eq "P" && $To eq $Board{FLAG}{passant}) {
-					delete %Board{&Enpass()};
+				my %Save= %BOARD;
+				$BOARD{$To}= delete $BOARD{$From};
+				if (uc($Piece) eq "P" && $To eq $BOARD{FLAG}{passant}) {
+					delete %BOARD{&Enpass()};
 					}
 				$Check= scalar (&Attacking(&WhereIs($King)));
-				$Board{$From}= delete $Board{$To};
-				%Board= %Save;
+				$BOARD{$From}= delete $BOARD{$To};
+				%BOARD= %Save;
 				}
 			if ($Check==0)	{ return 1; }
 			}
@@ -503,8 +443,136 @@ sub CanMove {
 	}
 
 
+sub BoardMove {
+	#REM: Makes move of pieces in the board and adjust flags.
+	my ($From, $To)= @_;
+
+	my $Piece= $BOARD{$From};
+	my $Steps= Steps($From, $To);
+	$BOARD{$To}= delete $BOARD{$From};
+	
+	#REM: Castling move rook.
+	if (uc($Piece) eq "K" && $Steps==2) {
+		if ($To eq "g1") { $BOARD{f1}= delete $BOARD{h1}; $BOARD{FLAG}{castling}=~ s/K//;}
+		if ($To eq "c1") { $BOARD{d1}= delete $BOARD{a1}; $BOARD{FLAG}{castling}=~ s/Q//;}
+		if ($To eq "g8") { $BOARD{f8}= delete $BOARD{h8}; $BOARD{FLAG}{castling}=~ s/k//;}
+		if ($To eq "c8") { $BOARD{d8}= delete $BOARD{a8}; $BOARD{FLAG}{castling}=~ s/q//;}
+		}
+
+	#REM: Passant move...
+	if (uc($Piece) eq "P" && $To eq $BOARD{FLAG}{passant}) {
+		delete $BOARD{&Enpass()};
+		}
+	$BOARD{FLAG}{passant}="-";
+	if (uc($Piece) eq "P" && $Steps==2) {
+		my $X= substr($From, 0, 1);
+		if ($From=~ /2$/) { $BOARD{FLAG}{passant}= $X."3"; }
+		if ($From=~ /7$/) { $BOARD{FLAG}{passant}= $X."6"; }
+		}
+
+	#REM: Toggle color turn...
+	if ($BOARD{FLAG}{active} eq "b") { $BOARD{FLAG}{fullmove}++; }
+	$BOARD{FLAG}{active}= $BOARD{FLAG}{active} eq "w" ? "b" : "w";
+	if (scalar @Pattern) { &Pattern(); }
+	return;
+	}
 
 
 
+#########
+# UTILS #
+#########
+sub BoardShow {
+	binmode(STDOUT, ":utf8");
+	print " -----------------\n";
+	for (my $Row=8; $Row>0; $Row--) {
+		print "$Row|";
+		foreach my $Col(("a".."h")) {
+			my $Piece=$BOARD{$Col.$Row} ? $BOARD{$Col.$Row} : " ";
+			if ($^O=~ /linux/i) {
+				$Piece=~ tr/KQRBNP/\N{U+2654}\N{U+2655}\N{U+2656}\N{U+2657}\N{U+2658}\N{U+2659}/;
+				$Piece=~ tr/kqrbnp/\N{U+265A}\N{U+265B}\N{U+265C}\N{U+265D}\N{U+265E}\N{U+265F}/;
+				}
+			print "$Piece|";
+			}
+		print "\n";
+		}
+	print " -----------------\n";
+	print "  a b c d e f g h\n";
+	return;
+	}
 
+
+sub PrintGame {
+	print "Event: $GAME{Event}\n";
+	print "Site: $GAME{Site}\n";
+	print "Date: $GAME{Date}\n";
+	print "Round: $GAME{Round}\n";
+	print "White: $GAME{White}\n";
+	print "Black: $GAME{Black}\n";
+	print "Result: $GAME{Result}\n";
+
+	my $Count;
+	foreach my $Mov (@{$GAME{ARRAY}}) {
+		my ($W, $B)= split(" ", $Mov);
+		$Count++;
+		print "$Count\t$W\t$B\n";
+		}
+	return;
+	}
+
+
+sub ShowData {
+	print "GAME #$GAME{GameNum}, line: $GAME{GameLin}\n";
+	&BoardShow();
+	print "PRIME(Original):\n";
+	print "$GAME{PRIME}\n";
+	print "MOVES(Cleaned):\n";
+	print "$GAME{MOVES}\n";
+	print "ARRAY(Internal):\n";
+	my $Count=0;
+	for my $Mov (@{$GAME{ARRAY}}) {
+		$Count++;
+		my ($Wht, $Blk)= split(" ", $Mov);
+		print "$Count. $Wht $Blk ";
+		}
+	print "\n";
+	return;
+	}
+
+
+sub Pattern {
+	if ($Pattern[0] ne $BOARD{FLAG}{active}) {return;}
+	#if ($Search{GameLimit}>$Search{GameFound}) { return; }
+	foreach my $Pat (@Pattern) {
+		if (length($Pat)<3) { next; }
+		my $Piece= substr($Pat, 0, 1);
+		my $Chr= length($Pat)==3 ? "" : substr($Pat, 1, 1);
+		my $Sqr= substr($Pat, -2);
+		if ($Chr eq "") {
+			if ($BOARD{$Sqr} ne $Piece) { return; }
+			}
+
+		if ($Chr=~ /[mxj]/) { #Can Move/Capture
+			my @From= &WhereIs($Piece);
+			my $Flag;
+			if ($Chr eq "x") {
+				my $Enemy= $BOARD{$Sqr};
+				if ($Enemy eq "") { return; }
+				if (&WhatColor($Piece) eq &WhatColor($Enemy)) { return; }
+				}
+
+			my $Jmp= $Chr eq "j" ? 1 : 0;
+			foreach my $Fro (@From) {
+				if (&CanMove($Fro, $Sqr, 1, $Jmp)) { $Flag++; } 
+				}
+			if ($Flag==0) { return; }
+			}
+		}
+	print "FOUND: Game #$GAME{GameNum}(L$GAME{GameLin}), move $BOARD{FLAG}{fullmove}$BOARD{FLAG}{active}\n";
+	&BoardShow();
+	print "\n";
+	$Search{GameLimit}++;
+	return;
+	}
 
